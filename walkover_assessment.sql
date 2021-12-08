d0314472dabe20e6c30447eab85b219f22bc6d5e
ankitraj7005 <90837985+ankitraj7005@users.noreply.github.com>
2021-12-08 12:51:48 +0530

    Update walkover_assessment.sql

--- walkover_assessment.sql
+++ walkover_assessment.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2021 at 06:47 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Dec 08, 2021 at 08:21 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -61,20 +61,7 @@ INSERT INTO `questions` (`id`, `question`, `option1`, `option2`, `option3`, `opt
 (17, 'Which of the following tag is used to define options in a drop-down selection list?', '<select>\r\n    ', '<list>\r\n   ', '<dropdown>\r\n   \r\n', '<option>', '<option>'),
 (18, 'The <hr> tag in HTML is used for -', 'new line\r\n    \r\n', 'vertical ruler\r\n    ', 'new paragraph\r\n    ', 'horizontal ruler', 'horizontal ruler'),
 (19, 'Which of the following HTML tag is used to display the text with scrolling effect?', '<marquee>\r\n    ', '<scroll>\r\n    \r\n', '<div>\r\n    ', 'None of the above', '<marquee>'),
-(20, 'Which of the following is the container for <tr>, <th>, and <td> ?', '<data>\r\n    \r\n', '<table>\r\n    ', '<group>\r\n   ', 'All of the above', '<table>'),
-(21, 'An HTML program is saved by using the ____ extension.', '.ht\r\n    ', '.html\r\n    ', '.hml\r\n    \r\n', 'None of the above', '.html'),
-(22, 'Flask is a web development framework created in ___________ language.', 'C\r\n', 'Java\r\n', 'Python\r\n', 'JavaScript', 'Python'),
-(23, 'Is the Flask framework open source?', 'TRUE', 'FALSE', 'Can be true or false', 'Can not say', 'TRUE'),
-(24, 'How to add the mailing feature in the Flask Application?', 'pip install Flask', 'pip install Flask-Mail', 'install Flask-Mail', 'pip Flask-Mail', 'pip install Flask-Mail'),
-(25, 'WSGI stands for the?', 'Write Server Gateway Interface', 'Web Static Gateway Interface', 'Web Server Gateway Interface', 'Web Server Gateway Interact', 'Web Server Gateway Interface'),
-(26, 'Flask default port is?', '2000', '3000', '4000', '5000', '5000'),
-(27, 'Flask default host is a localhost (127.0.0.1)', 'TRUE', 'FALSE', 'Can be true or false', 'Can not say', 'TRUE'),
-(28, 'Flask is called a?', 'miniframework', 'microframework', 'peraframework', 'nanoframework', 'microframework'),
-(29, 'Flask works with most of the RDBMSs, such as?', 'PostgreSQL', 'SQLite', 'MySQL', 'All of the above', 'All of the above'),
-(30, 'Armin Ronacher created the Flask framework.', 'TRUE', 'FALSE', 'Can be true or false', 'Can not say', 'TRUE'),
-(31, 'Is Python case sensitive when dealing with identifiers?', 'YES', 'NO', 'Machine dependent', 'none of the mentioned', 'YES'),
-(32, 'Why are local variable names beginning with an underscore discouraged?', 'they are used to indicate a private variables of a class', 'they confuse the interpreter', 'they are used to indicate global variables', 'they slow down execution', 'they are used to indicate a private variables of a class'),
-(33, 'Which of the following is true for variable names in Python?', 'unlimited length', 'all private members must have leading and trailing underscores', 'underscore and ampersand are the only two special characters allowed', 'none of the mentioned', 'unlimited length');
+(20, 'Which of the following is the container for <tr>, <th>, and <td> ?', '<data>\r\n    \r\n', '<table>\r\n    ', '<group>\r\n   ', 'All of the above', '<table>');
 
 -- --------------------------------------------------------
 
@@ -95,9 +82,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `marks`, `date`) VALUES
-(22, 'Ritik Dongre', 'ritikdongre1100@gmail.com', 7, '2021-08-04 08:01:58'),
-(23, 'Ritik Dongre', 'ritikdongre1100@gmail.com', 9, '2021-08-04 08:23:08'),
-(24, 'Ritik Dongre', 'ritikdongre1100@gmail.com', 5, '2021-08-04 09:19:47');
+(1, 'Ankit Raj', 'ankit@gmail.com', 4, '2021-12-07 13:43:37'),
+(2, 'wads', 'asdasd@dfs.com', 1, '2021-12-07 15:08:58'),
+(3, 'sfaada', 'asdasd@adsadd.com', 0, '2021-12-07 15:11:08'),
+(4, 'sdfsdf', 'dsfs@assfdf.com', 0, '2021-12-07 15:13:50'),
+(5, 'sdfsdf', 'dsfs@assfdf.com', 5, '2021-12-07 15:15:19'),
+(46, 'Nitish Kumar', 'nitish123@gmail.com', 2, '2021-12-08 12:47:36');
 
 --
 -- Indexes for dumped tables
@@ -129,7 +119,7 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
